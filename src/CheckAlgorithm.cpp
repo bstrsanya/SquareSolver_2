62512c997287b226dc9aa9648967a231f8dad988
bstrsanya <baas157@bk.ru>
2024-08-27 13:31:21 +0300

    8 версия
    Предпоследняя

--- src/CheckAlgorithm.cpp
+++ src/CheckAlgorithm.cpp
@@ -23,24 +23,25 @@ void CheckAlgorithm ()
 
     if (CompareDouble (check_algorithm, 1))
         {
-        //const int n_test = 14;
-        //                    #   a    b   c
-        //--------------------------------------------------------------------
-        Test data[] = {{1,  1,   2,  -3,   two_solve,  -3,  1},   // a = 1,   b = 2,   c = -3,   two_solve 
-                             {2,  1.5, 3,  -4.5, two_solve,  -3,  1},   // a = 1.6, b = 3.2, c = -4.8, two_solve
-                             {3,  -5,  6,  11,   two_solve,  2.2, -1}, // a = -5,  b = 6,   c = 11,   two_solve
-                             {4,  1,   2,  1,    one_solve,  -1,  -1},  // a = 1,   b = 2,   c = 1,    one_solve
-                             {5,  4,   8,  9,    no_solve,   0,   0},   // a = 4,   b = 8,   c = 9,    no_solve
+        //              #   a    b   c     nSolutions   x1  x2
+        //------------------------------------------------------------------------------------------------
+        Test data[] = {{1,  1,   2,  -3,   two_solve,  -3,  1},  // a = 1,   b = 2,   c = -3,   two_solve 
+                       {2,  1.5, 3,  -4.5, two_solve,  -3,  1},  // a = 1.6, b = 3.2, c = -4.8, two_solve
+                       {3,  -5,  6,  11,   two_solve,  2.2, -1}, // a = -5,  b = 6,   c = 11,   two_solve
+                       {4,  1,   2,  1,    one_solve,  -1,  -1}, // a = 1,   b = 2,   c = 1,    one_solve
+                       {5,  4,   8,  9,    no_solve,   0,   0},  // a = 4,   b = 8,   c = 9,    no_solve
 
-                             {6,  0,   0,  0,    more_solve, 0,  0},  // a = 0, b = 0,  c = 0,   more_solve 
-                             {7,  0,   5,  -10,  one_solve,  2,  2},  // a = 0, b = 5,  c = -10, one_solve 
-                             {8,  4,   0,  -16,  two_solve,  -2, 2},  // a = 4, b = 0,  c = -16, two_solve
-                             {9,  3,   27, 0,    two_solve,  -9, 0},  // a = 3, b = 27, c = 0,   two_solve
-                             {10, 0,   0,  3,    no_solve,   0,  0},  // a = 0, b = 0,  c = 3,   no_solve
-                             {11, 1,   0,  0,    one_solve,  0,  0},  // a = 1, b = 0,  c = 0,   one_solve
-                             {12, 0,   7,  0,    one_solve,  0,  0}}; // a = 0, b = 7,  c = 0,   one_solve
-        int n_test = sizeof(data) / sizeof(data[0]);
+                       {6,  0,   0,  0,    more_solve, 0,  0},  // a = 0, b = 0,  c = 0,   more_solve 
+                       {7,  0,   5,  -10,  one_solve,  2,  2},  // a = 0, b = 5,  c = -10, one_solve 
+                       {8,  4,   0,  -16,  two_solve,  -2, 2},  // a = 4, b = 0,  c = -16, two_solve
+                       {9,  3,   27, 0,    two_solve,  -9, 0},  // a = 3, b = 27, c = 0,   two_solve
+                       {10, 0,   0,  3,    no_solve,   0,  0},  // a = 0, b = 0,  c = 3,   no_solve
+                       {11, 1,   0,  0,    one_solve,  0,  0},  // a = 1, b = 0,  c = 0,   one_solve
+                       {12, 0,   7,  0,    one_solve,  0,  0}}; // a = 0, b = 7,  c = 0,   one_solve
+        
+        const int n_test = sizeof (data) / sizeof (data[0]);
         int counter = 0;                     
+        
         for (int i = 0; i < n_test; i++)
             {
             assert (0 <= i && i < n_test);
